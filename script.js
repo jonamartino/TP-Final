89de004d0894c767ac1ac7b131a7c9d0a833da40
Jonathan Martino <jonamartino@gmail.com>
2023-07-19 05:14:45 -0300

    Agregada funcionalidad de atacar fichas, moviendo la ficha y eliminando la anterior, falta agregar validacion en eatable

--- script.js
+++ script.js
@@ -54,17 +54,20 @@ for (let row = 0; row < boardSize; row++) {
       if (cell.classList.contains(currentPlayer)) {
         if (selectedCell && selectedCell !== cell) {
           selectedCell.classList.remove('clicked');
-          clearAllowedMoves();
+          clearHighlightMoves();
           selectedCell = cell;
           selectedCell.classList.add('clicked');
           console.log('Ficha seleccionada en la posición:', { x: col, y: row });
           const pieceColor = cell.classList.contains('black') ? 'black' : 'white';
           const possibleMoves = determinePossibleMoves(col, row, pieceColor);
+          const eatableMoves = determineEatableMoves(col, row, pieceColor);
+          console.log('Posibles Ataques:', eatableMoves);
           console.log('Posibles movimientos:', possibleMoves);
           highlightAllowedMoves(possibleMoves);
+          highlightEatableMoves(eatableMoves);
         } else if (selectedCell === cell) {
           selectedCell.classList.remove('clicked');
-          clearAllowedMoves();
+          clearHighlightMoves();
           selectedCell = null;
           console.log('Ficha deseleccionada');
         } else {
@@ -73,13 +76,19 @@ for (let row = 0; row < boardSize; row++) {
           console.log('Ficha seleccionada en la posición:', { x: col, y: row });
           const pieceColor = cell.classList.contains('black') ? 'black' : 'white';
           const possibleMoves = determinePossibleMoves(col, row, pieceColor);
+          const eatableMoves = determineEatableMoves(col, row, pieceColor);
+          console.log('Posibles Ataques:', eatableMoves);
           console.log('Posibles movimientos:', possibleMoves);
           highlightAllowedMoves(possibleMoves);
+          highlightEatableMoves(eatableMoves);
         }
       } else if (cell.classList.contains('allowed')) {
         const selectedPieceColor = selectedCell.classList.contains('black') ? 'black' : 'white';
         movePiece(selectedCell, cell, selectedPieceColor);
-      }
+      } else if (cell.classList.contains('eatable')) {
+        const selectedPieceColor = selectedCell.classList.contains('black') ? 'black' : 'white';
+        movePiece(selectedCell, cell, selectedPieceColor);
+      } 
     });
 
     const isInitialPiece = initialPiecePositions.some(position => position.x === col && position.y === row);
@@ -105,7 +114,10 @@ function determinePossibleMoves(selectedPieceX, selectedPieceY, pieceColor) {
       var possibleY = selectedPieceY + 1;
       if (possibleX >= 0 && possibleX < boardSize && possibleY >= 0 && possibleY < boardSize) {
         var possibleMove = { x: possibleX, y: possibleY };
-        possibleMoves.push(possibleMove);
+        const possibleCell = document.querySelector(`.cell[data-x="${possibleX}"][data-y="${possibleY}"]`);
+        if (!possibleCell.classList.contains('black') && !possibleCell.classList.contains('white')) {
+          possibleMoves.push(possibleMove);
+        }
       }
     }
   } else if (pieceColor === 'white') {
@@ -115,14 +127,74 @@ function determinePossibleMoves(selectedPieceX, selectedPieceY, pieceColor) {
       var possibleY = selectedPieceY - 1;
       if (possibleX >= 0 && possibleX < boardSize && possibleY >= 0 && possibleY < boardSize) {
         var possibleMove = { x: possibleX, y: possibleY };
-        possibleMoves.push(possibleMove);
+        const possibleCell = document.querySelector(`.cell[data-x="${possibleX}"][data-y="${possibleY}"]`);
+        if (!possibleCell.classList.contains('black') && !possibleCell.classList.contains('white')) {
+          possibleMoves.push(possibleMove);
+        }
       }
     }
   }
-
   return possibleMoves;
 }
+// Calcular las posiciones de movimiento de ataque de las fichas. Devuelve la posicion donde se movera la ficha.
+function determineEatableMoves(selectedPieceX, selectedPieceY, pieceColor) {
+  var eatableMoves = [];
+  var moveOffsets = [-1, 1];
+  if (pieceColor === 'black') {
+    // Movimiento para fichas negras (hacia abajo)
+    for (var yOffset of moveOffsets) {
+      var possibleX = selectedPieceX + yOffset;
+      var possibleY = selectedPieceY + 1;
+      if (possibleX >= 0 && possibleX < boardSize && possibleY >= 0 && possibleY < boardSize) {
+        var possibleMove = { x: possibleX, y: possibleY };
+        const possibleCell = document.querySelector(`.cell[data-x="${possibleX}"][data-y="${possibleY}"]`);
+        if (!possibleCell.classList.contains('black') && possibleCell.classList.contains('white')){
+          console.log("se puede comer la blanca en: ", possibleMove)
+          const killcell = possibleCell;
+          killcell.classList.add('kill');
+          var eatableX = possibleX + yOffset;
+          var eatableY = possibleY + 1;
+          var eatableMove = { x: eatableX, y: eatableY };
+          const eatableCell = document.querySelector(`.cell[data-x="${eatableX}"][data-y="${eatableY}"]`);
+          console.log(eatableMove);
+          if(eatableX >= 0 && eatableX < boardSize && eatableY >= 0 && eatableY < boardSize){
+            if(!eatableCell.classList.contains('black') && !eatableCell.classList.contains('white')){
+              console.log("POSICION DE COMER EN: ", eatableMove)
+              eatableMoves.push(eatableMove);
+            } 
+          }
 
+        }
+      } 
+    }
+  } else if (pieceColor === 'white') {
+  // Movimiento para fichas blancas (hacia arriba)
+  for (var yOffset of moveOffsets) {
+    var possibleX = selectedPieceX + yOffset;
+    var possibleY = selectedPieceY - 1;
+    if (possibleX >= 0 && possibleX < boardSize && possibleY >= 0 && possibleY < boardSize) {
+      var possibleMove = { x: possibleX, y: possibleY };
+      const possibleCell = document.querySelector(`.cell[data-x="${possibleX}"][data-y="${possibleY}"]`);
+      if (!possibleCell.classList.contains('white') && possibleCell.classList.contains('black')){
+        console.log("se puede comer la negra en: ", possibleMove)
+        const killcell = possibleCell;
+        killcell.classList.add('kill');
+        var eatableX = possibleX + yOffset;
+        var eatableY = possibleY - 1;
+        var eatableMove = { x: eatableX, y: eatableY };
+        const eatableCell = document.querySelector(`.cell[data-x="${eatableX}"][data-y="${eatableY}"]`);
+        console.log(eatableMove);
+        if(!eatableCell.classList.contains('black') && !eatableCell.classList.contains('white')){
+          console.log("POSICION DE COMER EN: ", eatableMove)
+          eatableMoves.push(eatableMove);
+        }
+      }
+    }
+  }
+  }
+
+  return eatableMoves;
+}
 // Resaltar los movimientos permitidos agregando la clase .allowed a las celdas correspondientes
 function highlightAllowedMoves(possibleMoves) {
   possibleMoves.forEach(move => {
@@ -134,17 +206,45 @@ function highlightAllowedMoves(possibleMoves) {
   });
 }
 
+// Resaltar los movimientos permitidos agregando la clase .allowed a las celdas correspondientes
+function highlightEatableMoves(eatableMoves) {
+  eatableMoves.forEach(move => {
+    const { x, y } = move;
+    const allowedCell = document.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
+    if (!allowedCell.classList.contains('black') && !allowedCell.classList.contains('white')) {
+      allowedCell.classList.add('eatable');
+    }
+  });
+}
+
 // Mover la pieza a la posición seleccionada
 function movePiece(sourceCell, targetCell, pieceColor) {
+  if(targetCell.classList.contains('eatable')){
+  const killCell = document.querySelector('.cell.kill');
+  killCell.classList.remove('black','white');
   targetCell.classList.add(pieceColor);
   targetCell.classList.remove('allowed');
+  targetCell.classList.remove('eatable');
   sourceCell.classList.remove(pieceColor);
   sourceCell.classList.remove('clicked');
-  clearAllowedMoves();
+  clearHighlightMoves();
+  selectedCell = null;
+  console.log('Pieza comida');
+  changeTurn(); // Cambiar el turno después de mover la pieza
+  updatePieceCount(); // Actualizar la información de la cantidad de fichas
+
+  }else {
+  targetCell.classList.add(pieceColor);
+  targetCell.classList.remove('allowed');
+
+  sourceCell.classList.remove(pieceColor);
+  sourceCell.classList.remove('clicked');
+  clearHighlightMoves();
   selectedCell = null;
   console.log('Pieza movida');
   changeTurn(); // Cambiar el turno después de mover la pieza
   updatePieceCount(); // Actualizar la información de la cantidad de fichas
+  }
 }
 
 // Cambiar el turno
@@ -174,10 +274,20 @@ function updatePieceCount() {
 }
 
 // Limpiar el resaltado de los movimientos permitidos eliminando la clase .allowed de todas las celdas
-function clearAllowedMoves() {
+function clearHighlightMoves() {
   const allowedCells = document.querySelectorAll('.cell.allowed');
+  const eatableCells = document.querySelectorAll('.cell.eatable');
+  const killCells = document.querySelectorAll('.cell.kill');
+  
+
   allowedCells.forEach(cell => {
     cell.classList.remove('allowed');
+  });
+  eatableCells.forEach(cell => {
+    cell.classList.remove('eatable');
+  });
+  killCells.forEach(cell => {
+    cell.classList.remove('kill');
   });
 }
 
